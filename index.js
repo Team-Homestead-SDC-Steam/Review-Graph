30c596e5974dd03b271da1f25dd912131eb5bdf9
Damien Jones <damienjones@gmail.com>
2020-06-27 01:04:58 -0700

    Passing JSON.parsed objects to res.json(), and fixing JSON formatting to use double-quotes

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ app.use(express.static('./client/dist'));
 app.get('/api/reviewcount/:gameId', (req, res) => {
   const sqlText = 'SELECT SUM(positive) as pos, SUM(negative) as neg '
                 + `FROM reviews_graph WHERE gameid = ${req.params.gameId};`;
-
   db.query(sqlText, (err, result) => {
     if (err) { res.status(500).send({ error: 'Internal server error' }); throw err; }
     const row = result[0];
@@ -20,8 +19,8 @@ app.get('/api/reviewcount/:gameId', (req, res) => {
     const { neg } = row;
     const tot = pos + neg;
     const per = Math.floor((pos / tot) * 100);
-    res.json(`{ summary: '${utils.rating(per)}', percent: ${per}, positive: ${pos}, negative: ${neg}, total: ${tot} }`);
-    // ex. { summary: 'Positive', percent: 82, positive: 4677, negative: 1218, total: 77856 }
+    res.json(JSON.parse(`{"summary": "${utils.rating(per)}", "percent": ${per}, "positive": ${pos}, "negative": ${neg}, "total": ${tot}}`));
+    // ex. {"summary":"Mixed","percent":64,"positive":3750,"negative":2059,"total":5809}
   });
 });
 
@@ -35,8 +34,8 @@ app.get('/api/reviewcount/recent/:gameId', (req, res) => {
     const { pos } = row;
     const tot = pos + row.neg;
     const per = Math.floor((pos / tot) * 100);
-    res.json(`{ summary: '${utils.rating(per)}', percent: ${per}, total: ${tot} }`);
-    // ex. { summary: 'Positive', percent: 82, positive: 4677, negative: 1218, total: 77856 }
+    res.json(JSON.parse(`{"summary": "${utils.rating(per)}", "percent": ${per}, "total": ${tot}}`));
+    // ex. {"summary":"Mixed","percent":63,"total":393}
   });
 });
 
@@ -44,11 +43,10 @@ app.get('/api/reviewcount/detail/:gameId', (req, res) => {
   const sqlText = 'SELECT CONCAT ( Year(date), \'-\', LPAD( Month(date), 2, \'0\'), \'-01\' ) as month, '
               + 'SUM(positive) as pos, SUM(negative) as neg FROM reviews_graph '
               + `WHERE gameid = ${req.params.gameId} GROUP BY month ORDER BY month;`;
-
   db.query(sqlText, (err, result) => {
     if (err) { res.status(500).send({ error: 'Internal server error' }); throw err; }
-    res.json(`{ [ ${result.map((row) => `{ month: '${row.month}', pos: ${row.pos}, neg: ${row.neg} }`).slice(0, -1)} ] }`);
-    // ex. { [ { month: '2019-10-01', positive: 4720, negative: 1591} ... ] }
+    res.json(JSON.parse(`{"detail": [${result.map((row) => `{"month": "${row.month}", "positive": ${row.pos}, "negative": ${row.neg} }`).slice(0, -1)}]}`));
+    // ex. {"detail":[{"month":"2019-06-01","pos":21,"neg":7},{},...]}
   });
 });
 
@@ -57,10 +55,9 @@ app.get('/api/reviewcount/recent/detail/:gameId', (req, res) => {
                 + 'SUM(positive) as pos, SUM(negative) as neg '
                 + 'FROM reviews_graph WHERE date >= CURDATE()-30 AND date <= CURDATE() '
                 + `AND gameid = ${req.params.gameId} GROUP BY day ORDER BY day;`;
-
   db.query(sqlText, (err, result) => {
     if (err) { res.status(500).send({ error: 'Internal server error' }); throw err; }
-    res.json(`{ [ ${result.map((row) => `{ day: '${row.day}', pos: ${row.pos}, neg: ${row.neg} }`).slice(0, -1)} ] }`);
-    // ex. {  [ {day: '2020-07-01', positive: 133, negative: 2}, … ] }
+    res.json(JSON.parse(`{"detail": [${result.map((row) => `{"day": "${row.day}", "positive": ${row.pos}, "negative": ${row.neg} }`).slice(0, -1)}]}`));
+    // ex. {"detail":[{"day":"2020-06-01","pos":16,"neg":4},{},...]}
   });
 });
