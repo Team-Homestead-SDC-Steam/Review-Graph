f1e47db7ccb2d9f4f39d89dcba49e3dcb51d8e4a
Damien Jones <damienjones@gmail.com>
2020-07-18 13:07:56 -0700

    Adjusting API calls & adding error handling

--- client/components/GraphRecent.jsx
+++ client/components/GraphRecent.jsx
@@ -11,11 +11,13 @@ export default class GraphRecent extends React.Component {
   componentDidMount() {
     // eslint-disable-next-line react/prop-types
     const { game } = this.props;
-    fetch(`/api/reviewcount/recent/detail/${game}`)
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({ data: json });
-      });
+    if (game !== '') {
+      fetch(`/api/reviews/recent/detail/${game}`)
+        .then((response) => response.json())
+        .then((json) => {
+          this.setState({ data: json });
+        });
+    }
   }
 
   render() {
